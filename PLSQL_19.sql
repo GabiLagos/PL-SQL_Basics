c47b2ba600e5d41f02f7eee81ee5c76ee4b2c625
GabiLagos <gabi.lagosvaldes@gmail.com>
2025-05-11 15:47:31 +0200

    Update PLSQL_19.sql

--- PLSQL_19.sql
+++ PLSQL_19.sql
@@ -26,20 +26,5 @@ update employees1 set last_name=upper(last_name) where salary = 17000;
 rollback;
 
 
---practica 19
-/*1. The rows in the JOBS table store a minimum and maximum salary allowed for different 
-JOB_ID values. You are asked to write code to ensure that employees’ salaries fall in the 
-range allowed for their job type, for insert and update operations. 
-a. Create a procedure called CHECK_SALARY as follows:  
-    1) The procedure accepts two parameters, one for an employee’s job ID string and 
-    the other for the salary.  
-    2) The procedure uses the job ID to determine the minimum and maximum salary for 
-    the specified job.
-    3) If the salary parameter does not fall within the salary range of the job, inclusive of 
-    the minimum and maximum, then it should raise an application exception, with the 
-    message “Invalid salary <sal>. Salaries for job <jobid> must 
-    be between <min> and <max>.”  Replace the various items in the message 
-    with values supplied by parameters and variables populated by queries. Save the 
-    file.
-    */
+
 
